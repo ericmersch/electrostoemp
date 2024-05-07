a93084247b9f4de71e13f9b54d86275b7fd9ea5c
ericmersch <58180106+ericmersch@users.noreply.github.com>
2024-05-07 08:40:33 -0700

    Update and rename didital_Input_to_MIDI_note.ino to digital_Input_to_MIDI_note.ino

--- digital_Input_to_MIDI_note.ino
+++ digital_Input_to_MIDI_note.ino
@@ -1,36 +1,4 @@
 #include "MIDIUSB.h"
-/*
-  State change detection (edge detection)
-
- ***********************************************************************
- **                                                                   **
- **  The "Bounce" library offers a much easier way to detect changes  **
- **                                                                   **
- ***********************************************************************
- 	
- Often, you don't need to know the state of a digital input all the time,
- but you just need to know when the input changes from one state to another.
- For example, you want to know when a button goes from OFF to ON.  This is called
- state change detection, or edge detection.
- 
- This example shows how to detect when a button or button changes from off to on
- and on to off.
- 	
- The circuit:
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- * LED attached from pin 13 to ground (or use the built-in LED on
-   most Arduino boards)
- 
- created  27 Sep 2005
- modified 30 Aug 2011
- by Tom Igoe
-
-This example code is in the public domain.
- 	
- http://arduino.cc/en/Tutorial/ButtonStateChange
- 
- */
 
 // this constant won't change:
 const int  buttonPin = 2;    // the pin that the pushbutton is attached to
