e0650c07866ec6a1dada0da6cc9b541126900dc1
ericmersch <58180106+ericmersch@users.noreply.github.com>
2024-05-08 12:12:05 -0700

    Update ADC_to_MIDIUSB.ino

--- ADC_to_MIDIUSB.ino
+++ ADC_to_MIDIUSB.ino
@@ -1,25 +1,27 @@
+#include <MIDIUSB.h>
 
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+// controlChange(0, 10, 65); // Set the value of controller 10 on channel 0 to 65
 
-#include "MIDIUSB.h"
-
-//Global Variables
-int sensorValue = 0;      //initialization of sensor variable, equivalent to EMA Y
- 
-
-void setup(){
-}
-
-
-void sendControlChange(byte channel, byte control, byte value) {
+void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
 }
- 
-void loop(){
-  sensorValue = analogRead(A0);    //read the sensor value using ADC
 
-  sendControlChange (0,32,map(sensorValue,0,1023,0,127));
-  MidiUSB.flush();
-   
-  delay(5);                              //20ms delay
+int lastPotValue=0;
+
+void setup() {
+}
+
+void loop() {
+  
+  int potValue = analogRead(A0);
+  if (lastPotValue != potValue){
+    controlChange(0, 50, map(potValue,0,1023,0,127));
+    
+  }
+  lastPotValue = potValue;
 }
