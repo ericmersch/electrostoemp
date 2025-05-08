9f0fe5feb64fc5ea8527e607c91c64478308fb29
ericmersch <eric.m.mersch@gmail.com>
2025-05-08 17:37:10 +0200

    Update ADC_to_MIDIUSB.ino

--- ADC_to_MIDIUSB.ino
+++ ADC_to_MIDIUSB.ino
@@ -1,28 +1,16 @@
 #include <MIDIUSB.h>
 
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
-// controlChange(0, 10, 65); // Set the value of controller 10 on channel 0 to 65
-
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
 }
 
-int lastPotValue=0;
-
 void setup() {
 }
 
 void loop() {
-  
-  int potValue = analogRead(A0);
-  if (lastPotValue != potValue){
-    controlChange(0, 50, map(potValue,0,1023,0,127));
-    MidiUSB.flush();
-  }
-  lastPotValue = potValue;
+  int sensorValue = analogRead(A0);
+  controlChange(0, 50, map(sensorValue,0,1023,0,127));
+  MidiUSB.flush();
   delay(20);
 }
