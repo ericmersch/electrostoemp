0da695cbfbdc42cd6e0573f3f0e2a2f3126d2465
ericmersch <eric.m.mersch@gmail.com>
2025-06-02 14:25:09 +0200

    Update heartrate_MIDIUSB.ino

--- heartrate_MIDIUSB.ino
+++ heartrate_MIDIUSB.ino
@@ -7,36 +7,34 @@ void controlChange(byte channel, byte control, byte value) {
 //Global Variables
 int sensorPin = 0;        //pin number to use the ADC
 int sensorValue = 0;      //initialization of sensor variable, equivalent to EMA Y
- 
-float a = 0.3;    //initialization of EMA alpha
+
+float a = 0.03;    //initialization of EMA alpha
 float b = 0.5;
- 
+
 float y1 = 0;        //initialization of EMA S
 float y2 = 0;
 
 float bandpass = 0;
- 
-void setup(){
-  Serial.begin(9600);                   //setup of Serial module, 115200 bits/second
-   
+
+void setup() {
+  Serial.begin(115200);                   //setup of Serial module, 115200 bits/second
+
   y1 = analogRead(sensorPin);      //set EMA S for t=1
   y2 = analogRead(sensorPin);
 }
- 
-void loop(){
-  sensorValue =0;
-  for(int i = 0; i<250; i++){
-  sensorValue += analogRead(sensorPin);    //read the sensor value using ADC
-  } 
-  y1 = a*sensorValue + (1-a)*y1;  //run the EMA
-  y2 = b*sensorValue + (1-b)*y2;
-  
+
+void loop() {
+  sensorValue = 0;
+  for (int i = 0; i < 250; i++) {
+    sensorValue += analogRead(sensorPin);    //read the sensor value using ADC
+  }
+  y1 = a * sensorValue + (1 - a) * y1; //run the EMA
+  y2 = b * sensorValue + (1 - b) * y2;
+
   bandpass = y2 - y1;      //find the band-pass
-  
-  controlChange (0, 32, max(0,atan((120+bandpass) / 120.)*80.8507)); //sigmoid and threshold
+
+  controlChange (0, 32, (PI/2+atan(bandpass / 400.))*127/PI); //sigmoid and threshold
   MidiUSB.flush();
 
-  Serial.println(max(0,atan((120+bandpass) / 120.)*80.8507));
-   
-  //delay(1);                              //20ms delay
+  Serial.println(bandpass);
 }
